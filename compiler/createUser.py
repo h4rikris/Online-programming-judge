efc58550996dbd8d82290406954e1cd066b74fca
HARI KRISHNA KANCHI <harry.iiit143@gmail.com>
2014-01-23 22:03:40 +0530

    commiting changes

---
+++ compiler/createUser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#Author	:	HARI KRISHNA K
+import os,sys
+contestpath="/home/krishna/Contest/"
+contest=sys.argv[1]
+pcode=sys.argv[2]
+user=sys.argv[3]
+l=os.listdir(contestpath)
+l1=os.listdir(contestpath+contest)
+l2=os.listdir(contestpath+contest+"/"+pcode)
+if (contest in l) and (pcode in l1):
+	if user not in l2:
+		os.mkdir(contestpath+contest+"/"+pcode+"/"+user)
+	print "1"
+else:
+	print "0"
